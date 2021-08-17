577db8a4ac5e9f370dfb1083d5fd020b4f020270
Lemonbrush <aa_rubtsov@mail.ru>
2021-08-17 21:27:34 +0300

    Исправил ДЗ по 5 лекции

--- Homework/Projects/StringCalculator/StringCalculator/main.swift
+++ Homework/Projects/StringCalculator/StringCalculator/main.swift
@@ -8,10 +8,47 @@
 import Foundation
 
 func sum(num1: String, num2: String) -> String {
-    if let safeNum1 = Int(num1), let safeNum2 = Int(num2) {
-        return String(safeNum1 + safeNum2)
-    } else {
+    
+    guard num1.isInt, num2.isInt else {
         return "Invalid input :("
+    }
+    
+    var firstValue = String(num1.reversed())
+    var secondValue = String(num2.reversed())
+    var separator = false
+    var result = ""
+    
+    if firstValue.count < secondValue.count {
+        swap(&firstValue, &secondValue)
+    }
+    
+    for (i, _) in firstValue.enumerated() {
+        let currentNum = firstValue.index(firstValue.startIndex, offsetBy: i)
+        var sum = (firstValue[currentNum].wholeNumberValue ?? 0)
+        
+        if secondValue.indices.contains(currentNum) {
+            sum += (secondValue[currentNum].wholeNumberValue ?? 0)
+        }
+        
+        if separator {
+            sum += 1
+        }
+        
+        separator = sum > 9
+        
+        result.append(String(sum % 10))
+    }
+    
+    if separator {
+        result.append("1")
+    }
+    
+    return String(result.reversed())
+}
+
+extension String {
+    var isInt: Bool {
+        return Int(self) != nil
     }
 }
 
@@ -21,6 +58,4 @@ print(sum(num1: "5", num2: "10"))
 
 print(sum(num1: "8", num2: "Ф"))
 
-print(sum(num1: "-8", num2: "7"))
-
-
+print(sum(num1: String(Int.max), num2: String(Int.max)))
