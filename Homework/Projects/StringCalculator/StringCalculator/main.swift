b474f9ae8b9115635b96cd7e0a5ed97cf75c0117
Lemonbrush <aa_rubtsov@mail.ru>
2021-08-19 20:20:14 +0300

    Поправил 5 дз

--- Homework/Projects/StringCalculator/StringCalculator/main.swift
+++ Homework/Projects/StringCalculator/StringCalculator/main.swift
@@ -9,10 +9,6 @@ import Foundation
 
 func sum(num1: String, num2: String) -> String {
     
-    guard num1.isInt, num2.isInt else {
-        return "Invalid input :("
-    }
-    
     var firstValue = String(num1.reversed())
     var secondValue = String(num2.reversed())
     var separator = false
@@ -59,3 +55,7 @@ print(sum(num1: "5", num2: "10"))
 print(sum(num1: "8", num2: "Ф"))
 
 print(sum(num1: String(Int.max), num2: String(Int.max)))
+
+print(sum(num1: "1126351526125351621621261", num2: "123212136263623127182182818218212121212") == "123212136263624253533708943569833742473")
+print(sum(num1: "117374723923829427492472947275295385487584757457482032032490243584353485384204",
+   num2: "123485438574385734579823759823749832478324789327498237489237498324982374892374237") == "123602813298309564007316232771025127863812374084955719521269988568566728377758441")
